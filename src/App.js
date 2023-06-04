ce1c1374324a73bea65ee4865198fe66cc731f26
Greviansonula <greviansonula@gmail.com>
2023-06-04 08:07:43 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import MyButton from './testButton'
+
+// function MyButton() {
+//     return ( <
+//         button > I am a Button < /button>
+//     );
+// }
+
+function App() {
+    return ( <
+        div >
+        <
+        h1 > Welcome to my App < /h1> <
+        MyButton / >
+        <
+        /div>
+    );
+}
+
+export default App;
